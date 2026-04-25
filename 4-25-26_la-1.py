e0a28279e22a21dfee08d98018df6098bafd771b
NeilKristoph <nktagavilla@gmail.com>
2026-04-25 14:15:50 +0800

    Add my_info function to display personal information

---
+++ 4-25-26_la-1.py
@@ -0,0 +1,9 @@
+def my_info():
+    print("Name: Tagavilla, Neil Kristoph C.")
+    print("Section: BSIT 1B-NE")
+    print("Hobbies: Badminton, Online Games, Motorcycle, Gym")
+    print()
+
+
+my_info()
+my_info()
